0d32357f7d8caa03bb5e1b03784662643080ddd1
puzanovkirill <austinscottbeatz@gmail.com>
2021-11-09 16:50:34 +0500

    feat: new note button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,9 @@ const Navbar = () => {
          <div className="flex w-2/3">
             <h1 className="inline text-4xl font-bold">Notes</h1>
             <Search />
+            <button className="h-8 w-8 bg-gray-100 ml-2 self-center rounded-md">
+               <i className="fa fa-plus"></i>
+            </button>
          </div>
          <button className="text-xl pl-2 pr-2 ease-in-out duration-700">
             Dark mode
