af9b9079e82fcc9befb87419ff88f086239f2261
puzanovkirill <austinscottbeatz@gmail.com>
2021-11-09 16:36:23 +0500

    fix: modal window divs replaced with input/textarea

--- src/components/ModalWindow.jsx
+++ src/components/ModalWindow.jsx
@@ -1,29 +1,25 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
 
-const ModalWindow = () => {
+const ModalWindow = ({ note }) => {
    const [isOpen, setIsOpen] = useState(true);
 
    Modal.setAppElement(document.getElementById('root'));
    return (
-      <Modal
-         className="modal"
-         isOpen={isOpen}
-         style={{
-            content: {},
-         }}
-      >
+      <Modal className="modal" isOpen={isOpen}>
          <div className="p-8 h-full">
             <div className="flex justify-between">
-               <div className="text-4xl">some text</div>
+               <input
+                  maxLength={20}
+                  placeholder="header..."
+                  className="text-4xl outline-none"
+               />
                <button className="" onClick={() => setIsOpen(false)}>
                   <i className="fa fa-close"></i>
                </button>
             </div>
             <hr className="mt-2" />
-            <div className="h-4/5 text-2xl break-all overflow-y-scroll">
-               note text here
-            </div>
+            <textarea className="h-4/5 text-2xl break-all overflow-y-scroll w-full resize-none outline-none" />
             <hr className="mt-2" />
          </div>
       </Modal>
